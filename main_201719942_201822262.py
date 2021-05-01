77fa0e6c3fcf05e21e0dfb9bd13a9f610c42763e
Manuel Gallegos <m.gallegos@uniandes.edu.co>
2021-05-01 00:54:57 -0500

    actualizacion

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -5,6 +5,7 @@
 import os
 import glob
 import numpy as np
+import sklearn as sk
 from skimage import io
 from skimage import color
 from skimage import img_as_float
@@ -15,10 +16,10 @@ def calculate_descriptors(data, parameters):
         data = list(map(parameters['transform_color_function'], data))
     bins = [parameters['bins']]*len(data)
     histograms = list(map(parameters['histogram_function'], data, bins))
-    descriptor_matrix = np.array(histograms) 
+    descriptor_matrix = np.array(histograms)
     # TODO Verificar tamaño de descriptor_matrix a # imágenes x dimensión del descriptor
     return descriptor_matrix
-  
+
 def train(parameters, action):
     data_train = os.path.join('data_mp4', 'scene_dataset', 'train', '*.jpg')
     images_train = list(map(io.imread, glob.glob(data_train)))
@@ -29,10 +30,12 @@ def train(parameters, action):
     else:
         # Esta condición solo la tendrán que utilizar para la tercera entrega.
         # TODO Cargar matrices de parameters['train_descriptor_name']
-        carga = np.load(parameters['train_descriptor_name'])
+        descriptors = np.load(parameters['train_descriptor_name'])
     # TODO Definir una semilla y utilice la misma para todos los experimentos de la entrega.
+    entrenamiento = sk.cluster.k_means(descriptors, parameters['k'], random_state=0)
     # TODO Inicializar y entrenar el modelo con los descriptores.
     # TODO Guardar modelo con el nombre del experimento: parameters['name_model']
+    np.save(parameters['name_model'], entrenamiento)
 
 def validate(parameters, action):
     data_val = os.path.join('data_mp4', 'scene_dataset', 'val', '*.jpg')
@@ -40,15 +43,23 @@ def validate(parameters, action):
     if action == 'load':
         # Esta condición solo la tendrán que utilizar para la tercera entrega.
         # TODO Cargar matrices de parameters['val_descriptor_name']
+        descriptors = np.load(parameters['val_descriptor_name'])
     else:
         descriptors = calculate_descriptors(images_val, parameters)
         if action == 'save':
             # TODO Guardar matriz de descriptores con el nombre parameters['val_descriptor_name']
-        
+            np.save(parameters['val_descriptor_name'], descriptors)
     # TODO Cargar el modelo de parameters['name_model']
+    modelo = np.load(parameters['name_model'])
     # TODO Obtener las predicciones para los descriptores de las imágenes de validación
-    # TODO Obtener las métricas de evaluación
 
+    # TODO Obtener las métricas de evaluación
+    anotaciones = []
+    predicciones = []
+    conf_mat = sk.metrics.confusion_matrix(anotaciones, predicciones)
+    precision = sk.metrics.precision_score(anotaciones, predicciones)
+    recall = sk.metrics.recall_score(anotaciones, predicciones)
+    f_score = sk.metrics.f1_score(anotaciones, predicciones)
     return conf_mat, precision, recall, f_score
 
 def main(parameters, perform_train, action):
@@ -57,6 +68,10 @@ def main(parameters, perform_train, action):
     conf_mat, precision, recall, f_score = validate(parameters, action = action)
     #TODO Imprimir de manera organizada el resumen del experimento.
     # Incluyan los parámetros que usaron y las métricas de validación.
+    resp = f"Parámetros:\nEspacio: {parameters['space']}\nNúmero de Bins: {parameters['bins']}\nNúmero de clusters: " \
+           f"{parameters['k']}\nNombre del modelo: {parameters['name_model']}\nNombre del descriptor entrenamiento:" \
+           f"{parameters['train_descriptor_name']}\nNombre del descriptor validación: {parameters['val_descriptor_name']}" \
+           f"\nMatriz de confusión: {conf_mat}\nPrecisión: {precision}\nCobertura: {recall}\nF-score: {f_score}"
 
 if __name__ == '__main__':
     """
@@ -68,17 +83,23 @@ if __name__ == '__main__':
     Rogamos no hacer uso de este código por fuera del curso y de este semestre.
     ----------NO OPEN ACCESS!!!!!!!------------
     """
+    numero_bins = 120
+    numero_cluster = 8
+    nombre_modelo = 'modelo1.npy'
+    nombre_entrenamiento = 'entrenamiento1.npy'
+    nombre_validacion = 'validacion1.npy'
     # TODO Establecer los valores de los parámetros con los que van a experimentar.
     # Nota: Tengan en cuenta que estos parámetros cambiarán según los descriptores
     # y clasificadores a utilizar.
-    parameters= {'histogram_function': JointColorHistogram, 
+    parameters= {'histogram_function': JointColorHistogram,
              'space': 'HSV', 'transform_color_function': color.rgb2hsv, # Esto es solo un ejemplo.
              'bins': numero_bins, 'k': numero_cluster,
-             'name_model': '', # No olviden establecer la extensión con la que guardarán sus archivos. 
-             'train_descriptor_name': '', # No olviden asignar un nombre que haga referencia a sus experimentos y que corresponden a las imágenes de entrenamiento.
-             'val_descriptor_name': ''} # No olviden asignar un nombre que haga referencia a sus experimentos y que corresponden a las imágenes de validación. 
+             'name_model': nombre_modelo, # No olviden establecer la extensión con la que guardarán sus archivos.
+             'train_descriptor_name': nombre_entrenamiento, # No olviden asignar un nombre que haga referencia a sus experimentos y que corresponden a las imágenes de entrenamiento.
+             'val_descriptor_name': nombre_validacion} # No olviden asignar un nombre que haga referencia a sus experimentos y que corresponden a las imágenes de validación.
 
     perform_train = True # Cambiar parámetro a False al momento de hacer la entrega
-    action = 'save' # Cambiar a None al momento de hacer la entrega 
+    action = 'save' # Cambiar a None al momento de hacer la entrega
     main(parameters=parameters, perform_train=perform_train, action=action)
+
 
